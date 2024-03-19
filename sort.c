811da891f2915fc96c60e98b65dfccf8b0f3ee28
Deia Hamad <deia.tech2021@gmail.com>
2024-03-19 17:22:09 +0400

    done with sort_2, 3 & 5

--- sort.c
+++ sort.c
@@ -6,19 +6,19 @@
 /*   By: dehamad <dehamad@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 05:55:31 by dehamad           #+#    #+#             */
-/*   Updated: 2024/03/19 10:46:50 by dehamad          ###   ########.fr       */
+/*   Updated: 2024/03/19 17:21:29 by dehamad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	sort_2(t_stack **stack_a)
+static void	sort_two(t_stack **stack_a)
 {
 	if ((*stack_a)->content > (*stack_a)->next->content)
 		sa(stack_a);
 }
 
-static void	sort_3(t_stack **stack_a)
+static void	sort_three(t_stack **stack_a)
 {
 	int	num1;
 	int	num2;
@@ -31,10 +31,10 @@ static void	sort_3(t_stack **stack_a)
 		ra(stack_a);
 	else if (num2 == max)
 		rra(stack_a);
-	sort_2(stack_a);
+	sort_two(stack_a);
 }
 
-static void	sort_5(t_stack **stack_a, t_stack **stack_b)
+static void	sort_five(t_stack **stack_a, t_stack **stack_b)
 {
 	int		min;
 	t_stack	*last_node;
@@ -52,7 +52,7 @@ static void	sort_5(t_stack **stack_a, t_stack **stack_b)
 		else
 			ra(stack_a);
 	}
-	sort_3(stack_a);
+	sort_three(stack_a);
 	pa(stack_b, stack_a);
 	pa(stack_b, stack_a);
 }
@@ -65,15 +65,15 @@ void	sort(t_stack **stack_a, t_stack **stack_b)
 		exit_success(*stack_a, *stack_b);
 	size = ft_lstsize(*stack_a);
 	if (size == 2)
-		sort_2(stack_a);
+		sort_two(stack_a);
 	else if (size == 3)
-		sort_3(stack_a);
-	else if (size == 5)
-		sort_5(stack_a, stack_b);
+		sort_three(stack_a);
+	else if (size <= 5)
+		sort_five(stack_a, stack_b);
 	if (ft_lstiter(*stack_a, stack_is_sorted))
 		exit_success(*stack_a, *stack_b);
 	// else if (size <= 100)
 	// 	sort_hundred(stack_a, stack_b);
 	// else
-	// 	sort_5_hundred(stack_a, stack_b);
+	// 	sort_five_hundred(stack_a, stack_b);
 }
